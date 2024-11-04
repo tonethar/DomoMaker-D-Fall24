56abe0a5bf8428a8102c160d48c26d4b19439252
tonethar <tonethar@users.noreply.github.com>
2024-11-04 09:58:16 -0500

    DomoMaker C first commit

--- server/app.js
+++ server/app.js
@@ -1,3 +1,6 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+require('dotenv').config();
+
 const path = require('path');
 const express = require('express');
 const compression = require('compression');
@@ -7,47 +10,56 @@ const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
 const helmet = require('helmet');
 const session = require('express-session'); // B
+const RedisStore = require('connect-redis').default; // C
+const redis = require('redis'); // C
 
 const router = require('./router.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-const app = express();
+// const dbURI = process.env.MONGODB_URI || 'mongodb+srv://tony:uYxNkEthmiKwb0fR@cluster0.780sy.mongodb.net/DomoMaker';
+const dbURI = process.env.MONGODB_URI || require('../.mongo-connection.js');
 
-app.use(helmet());
-app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
-app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
-app.use(compression());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// B
-app.use(session({
-  key: 'sessionid',
-  secret: 'Domo Atigato',
-  resave: false,
-  saveUninitialized: false,
-}));
-//
-app.engine('handlebars', expressHandlebars.engine({ defaultLayout: '' }));
-app.set('view engine', 'handlebars');
-app.set('views', `${__dirname}/../views`);
+// C
+const redisClient = redis.createClient({
+  url: process.env.REDISCLOUD_URL,
+});
+redisClient.on('error', (err) => console.log('Redis Client Error', err));
 
-router(app);
+redisClient.connect().then(() => {
+  const app = express();
 
-const startServer = () => {
+  app.use(helmet());
+  app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
+  app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
+  app.use(compression());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  // B
+  app.use(session({
+    key: 'sessionid',
+    store: new RedisStore({
+      client: redisClient,
+    }),
+    secret: 'Domo Atigato',
+    resave: false,
+    saveUninitialized: false,
+  }));
+  //
+  app.engine('handlebars', expressHandlebars.engine({ defaultLayout: '' }));
+  app.set('view engine', 'handlebars');
+  app.set('views', `${__dirname}/../views`);
+
+  router(app);
   app.listen(port, (err) => {
     if (err) { throw err; }
     console.log(`Listening on port ${port}`);
   });
-};
-
-// const dbURI = process.env.MONGODB_URI || 'mongodb+srv://tony:uYxNkEthmiKwb0fR@cluster0.780sy.mongodb.net/DomoMaker';
-const dbURI = process.env.MONGODB_URI || require('../.mongo-connection.js');
+});
 
 mongoose.connect(dbURI)
   .then((instance) => {
     console.log(`Success - mongoose version = ${instance.version}`);
-    startServer();
   })
   .catch((err) => {
     if (err) {
