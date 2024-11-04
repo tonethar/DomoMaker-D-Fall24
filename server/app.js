5149587f1fd1b61c43cf7271eb083fd8fc7353d4
tonethar <tonethar@users.noreply.github.com>
2024-11-04 10:10:26 -0500

    added MONGODB_URI value

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ const router = require('./router.js');
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 // const dbURI = process.env.MONGODB_URI || 'mongodb+srv://tony:uYxNkEthmiKwb0fR@cluster0.780sy.mongodb.net/DomoMaker';
-const dbURI = process.env.MONGODB_URI || require('../.mongo-connection.js');
+const dbURI = process.env.MONGODB_URI; // || require('../.mongo-connection.js');
 
 // C
 const redisClient = redis.createClient({
